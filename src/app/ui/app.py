20bd40afc88a32dda9718c6906159b4ab4b40ecc
Arthurkph <kempnicharthur@gmail.com>
2026-02-26 09:17:36 +0100

    setup ui pas comme le figma / BFS / DFS / CLEAR CANVAS / SHOW INFO

--- src/app/ui/app.py
+++ src/app/ui/app.py
@@ -7,7 +7,7 @@ Palier F - Séances 6-8.
 """
 
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, simpledialog
 from ..core import Graph,save_graph,load_graph
 
 
@@ -31,7 +31,16 @@ class GraphExplorerApp:
         """
         self.root = root
         self.root.title("Explorateur de Graphes - ESEO S4")
-        self.root.geometry("1000x700")
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        width = int(screen_width * 0.8)
+        height = int(screen_height * 0.8)
+
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
         
         # Graphe actuel
         self.graph = Graph()
@@ -49,15 +58,54 @@ class GraphExplorerApp:
         # 4. Frame bas : zone de status/log
         
         # Exemple de structure de base:
-        #top_frame = tk.Frame(self.root)
-        #top_frame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        # top_frame = tk.Frame(self.root)
+        # top_frame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
         # 
-        #tk.Button(top_frame, text="Nouveau", command=self.new_graph).pack(side=tk.LEFT)
-        #tk.Button(top_frame, text="Charger", command=self.load_graph).pack(side=tk.LEFT)
-        #tk.Button(top_frame, text="Sauver", command=self.save_graph).pack(side=tk.LEFT)
+        # tk.Button(top_frame, text="Nouveau", command=self.new_graph).pack(side=tk.LEFT)
+        # tk.Button(top_frame, text="Charger", command=self.load_graph).pack(side=tk.LEFT)
+        # tk.Button(top_frame, text="Sauver", command=self.save_graph).pack(side=tk.LEFT)
         # ...
-        
-        pass
+
+
+        """GRILLE PRINCIPALE"""
+        self.root.grid_rowconfigure(1, weight=1)      # Ligne centrale extensible
+        self.root.grid_columnconfigure(1, weight=1)   # Colonne centre extensible
+
+        "FRAME HAUT"
+        top_frame = tk.Frame(self.root, bg="#00ff00")
+        top_frame.grid(row=0, column=0, columnspan=2, sticky="ew")
+
+        tk.Button(top_frame, text="Nouveau", command=self.new_graph).pack(side=tk.LEFT, padx=5, pady=5)
+        tk.Button(top_frame, text="Charger", command=self.load_graph).pack(side=tk.LEFT, padx=5, pady=5)
+        tk.Button(top_frame, text="Sauver", command=self.save_graph).pack(side=tk.LEFT, padx=5, pady=5)
+        tk.Button(top_frame, text="DFS", command=self.run_dfs).pack(side=tk.LEFT, padx=5, pady=5)
+        tk.Button(top_frame, text="BFS", command=self.run_bfs).pack(side=tk.LEFT, padx=5, pady=5)
+
+        """FRAME GAUCHE"""
+        left_frame = tk.Frame(self.root, bg="#2200ff", width=200)
+        left_frame.grid(row=1, column=0, sticky="ns")
+
+        tk.Label(left_frame, text="Nœuds du graphe").pack(pady=5)
+
+        self.node_listbox = tk.Listbox(left_frame)
+        self.node_listbox.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        """FRAME CENTRE"""
+        center_frame = tk.Frame(self.root)
+        center_frame.grid(row=1, column=1, sticky="nsew")
+
+        self.canvas = tk.Canvas(center_frame, bg="white")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+        """FRAME BAS"""
+        bottom_frame = tk.Frame(self.root, bg="#ff0000", height=120)
+        bottom_frame.grid(row=2, column=0, columnspan=2, sticky="ew")
+
+        tk.Label(bottom_frame, text="Logs / Status").pack(anchor="w")
+
+        self.log_text = tk.Text(bottom_frame, height=5)
+        self.log_text.pack(fill=tk.X, padx=5, pady=5)
+
     
     def new_graph(self):
         """Crée un nouveau graphe vide."""
@@ -110,23 +158,44 @@ class GraphExplorerApp:
         # TODO: implémenter
         # Astuce : appeler core.algorithms.dfs()
         # puis render.py pour visualiser
-        pass
+        start = simpledialog.askstring("DFS", "Noeud de départ :")
+
+        if start not in self.graph.nodes:
+            messagebox.showerror("Erreur", "Noeud Invalide")
+            return 
+        
+        result = self.graph.dfs(start)
+
+        self.log(f"DFS depuis {start} : {'->'.join(result)}")
     
+
     def run_bfs(self):
         """Lance BFS et visualise le résultat."""
-        # TODO: implémenter
-        pass
+        start = simpledialog.askstring("BFS", "Noeud de départ :")
+
+        if start not in self.graph.nodes:
+            messagebox.showerror("Erreur", "Noeud Invalide")
+            return 
+        
+        result = self.graph.Bfs(start)
+
+        self.log(f"BFS depuis {start} : {'->'.join(result)}")
+
     
     def clear_canvas(self):
         """Efface le canvas."""
-        # TODO: implémenter
-        pass
+        self.canvas.delete("all")
+        self.log("Canvas effacé")
     
     def show_info(self):
         """Affiche des infos sur le graphe actuel."""
         # TODO: implémenter
         # Exemple : nombre de nœuds, arêtes, connexité...
-        pass
+        nb_nodes = len(self.graph.nodes)
+        nb_edges = len(self.graph.edges)
+
+        infos = f""" Nombre de Noeuds : {nb_nodes}  , Nombre d'arrêtes : {nb_edges} """
+        messagebox.showinfo("Info du graph", infos)
 
 
 def main():

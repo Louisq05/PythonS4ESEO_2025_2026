dcb84acf671a96aa203f6c83ca6c5e20d639ca04
Bacalhau <louisquibeufworkspace@gmail.com>
2026-04-27 13:22:34 +0200

    fix/useless files deleting

--- src/app/ui/__init__.py
+++ src/app/ui/__init__.py
@@ -5,8 +5,6 @@ Interface graphique de l'application.
 """
 
 from .app import GraphExplorerApp, main
-from .controller import GraphController
-from .render import draw_graph, highlight_path, auto_layout
 
 __all__ = [
     "GraphExplorerApp",
